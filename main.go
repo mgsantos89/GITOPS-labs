91f5f33dac5ddb53882205aad98861ffc05f1ae3
Marco Gomes <mgsantos89@gmail.com>
2024-02-26 09:03:05 -0300

    Update Go App

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main(){
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Ola, eu sou o Marco Gomes!!!"))
+		w.Write([]byte("Ola, eu sou o Homem de Ferro!!!"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
